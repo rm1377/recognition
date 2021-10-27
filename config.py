3fa3f310b308a0104872a046ecdb5adbc3311e7c
rm1377 <52181737+rm1377@users.noreply.github.com>
2021-10-28 00:11:55 +0330

    first adding files

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from pathlib import Path
+from torchvision import transforms as trans
+
+batch_size = 64
+epochs = 10
+lr = 0.1
+momentum = 0.9
+save_model = True
+num_workers = 2
+input_size = 112
+embedding_size = 512
+
